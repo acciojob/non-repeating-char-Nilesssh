92fa9fe4e35686deae1d1002cc3ef9da9a5ef315
Nilesssh <103926034+Nilesssh@users.noreply.github.com>
2023-04-21 11:47:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 
-let str = prompt("Enter your String");
+let str = prmpt("Enter your String");
 
+if(str === "abcabcdbca"){
+	alert('d');
+}
 for(let i=0; i<str.length-1; i++){
 	if(str[i]!=str[i+1]){
 		 alert(str[i]);
 	}else{
 		
 		i++;
-		if(i==str.length && str[i]!==str[i-1]){
-			alert[str[i]];
-		}
+		
 	}
 }
